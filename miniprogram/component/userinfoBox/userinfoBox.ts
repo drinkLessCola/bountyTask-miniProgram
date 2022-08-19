5ef7f46f7c33c4af69df7a768e84161efa934c9e
lzh42 <1178074611@qq.com>
2022-08-20 01:32:54 +0800

    界面-任务详情(未完成)

--- miniprogram/component/userinfoBox/userinfoBox.ts
+++ miniprogram/component/userinfoBox/userinfoBox.ts
@@ -3,18 +3,23 @@ Component({
   /**
    * 组件的属性列表
    */
+  externalClasses:['fontCSS','imageCSS'],
+
   properties: {
     avatarUrl: String,
     nickname: String,
+    size:Number
   },
 
-  externalClasses: ['fontCSS'],
+  
   /**
    * 组件的初始数据
    */
   data: {
     avatarUrl: '',
-    nickname: ''
+    nickname: '',
+    size:50,
+    fontTop:20,
   },
 
   /**
@@ -22,12 +27,21 @@ Component({
    */
   lifetimes: {
     attached: function () {
+      let size = this.properties.size
+      if(size==null ||size==0){
+        size=50
+      }
+      let top = -size/2+14
+      //位移为框高一半 - 字体大小的一半
       this.setData({
         avatarUrl: this.properties.avatarUrl,
-        nickname: this.properties.nickname
+        nickname: this.properties.nickname,
+        size:size,
+        fontTop:top
       })
-      console.log(this.properties.avatarUrl);
-      console.log(this.data.nickname);
+
+      // console.log(this.properties.avatarUrl);
+      // console.log(this.data.nickname);
       
     }
   },
--- miniprogram/pages/taskDetail/taskDetail.ts
+++ miniprogram/pages/taskDetail/taskDetail.ts
@@ -10,10 +10,70 @@ Page({
     // avatarUrl: getApp().globalData.defaultAvatarUrl,
     avatarUrl:'http://tmp/ny1Hd2Fgrql4d21c915c93b7255357d06571d729118e.jpeg',
     // lzh的头像
-    nickname:'昵称'
+    //当前用户的
+    nickname:'昵称',
+    userid:0,
 
+    isPublisher:0,
+    // 0是发布者 1不是
+
+    publish:{
+      Id:0,
+      nickname:'发布者',
+      avatarUrl:'http://tmp/ny1Hd2Fgrql4d21c915c93b7255357d06571d729118e.jpeg',
+    },
     
+    geter:{
+      Id:0,//id最终不会放进task
+      nickname:'',
+      avatarUrl:'',
+      status:0,//2：待确认；1：进行中；0：已完成
+    },
+    // 领取者获取回来时的对象结构，仅用作展示
 
+    task:{
+      id:0,
+      title:'任务标题',
+      getCondition:[{
+        nickname:'1号',
+        avatarUrl:'http://tmp/ny1Hd2Fgrql4d21c915c93b7255357d06571d729118e.jpeg',
+        status:"已完成",
+      },{
+        nickname:'2号',
+        avatarUrl:'http://tmp/ny1Hd2Fgrql4d21c915c93b7255357d06571d729118e.jpeg',
+        status:"待确认",
+      },{
+        nickname:'3号',
+        avatarUrl:'http://tmp/ny1Hd2Fgrql4d21c915c93b7255357d06571d729118e.jpeg',
+        status:"进行中",
+      }],//这个需要二次请求才会处理好放进来
+      illstrate:'你猜',
+      request:'截图 / 拍照',
+      number:1,//这个是剩余n份任务
+      labels:['泰山区','紧急','校外'],
+      deadline:'2022年8月16日 上午 08:00',//yyyy年MM月dd日 上/下午 hh:mm 这里月份的十位若是0则不显示
+      bounty:'2',//这个是每份的 
+
+    },
+    // 这里的task对象是需要在下面处理后再放数据的
+    // 上一个界面传任务id和userid过来
+    // 调用接口：根据任务id获得任务发布者id，任务执行者id，以及其在该任务的状态  Ⅱ
+    // 比较userid和任务发布者id 决定这个页面显示什么
+    // 调用接口: 根据任务id查询返回任务对象
+    // 根据返回的任务对象 把所需要的信息填入这里的task对象
+
+    //领取情况：接口Ⅱ 还会返回所有任务执行id和执行状态,遍历所有执行者id,使用 接口:根据用户id查询 获取用户昵称和用户头像
+    //然后把这些用户昵称 头像 和 转成字符串的执行状态 打包成对象丢进去getCondition
+
+    //剩余n份任务等后端改完任务对象再看
+
+    //这里需要写一个字符串分离方法,因为后端的labels是个字符串而不是字符串数组
+    //要是有大聪明自定义标签是个','就寄了
+
+    //时间从任务对象里获取后要处理
+
+    //
+    
 
   },
   
